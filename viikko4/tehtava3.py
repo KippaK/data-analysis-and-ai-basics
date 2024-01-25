62bb2159505f2d4b38e83a68e4e58c2c5d14260c
KippaK <antti.venetjoki@gmail.com>
2024-01-25 15:40:45 +0200

    Viikko 4 valmis

--- viikko4/tehtava3.py
+++ viikko4/tehtava3.py
@@ -10,33 +10,31 @@ g_str = {0: 'miehet', 1:'naiset'}
 pie = plt.pie(x=g_counts.values, labels=g_str.values(), autopct='%.1f%%')
 plt.show()
 
-age_group_m_counts = pd.Series(df[df['gender']==0]['age_group'].value_counts()).reset_index().sort_values('age_group')
-age_group_f_counts = pd.Series(df[df['gender']==1]['age_group'].value_counts()).reset_index().sort_values('age_group')
+age_group_m_counts = df[df['gender']==0]['age_group'].value_counts().reset_index().sort_values('age_group')
+age_group_f_counts = df[df['gender']==1]['age_group'].value_counts().reset_index().sort_values('age_group')
+age_group_m_counts.rename(columns={'count':'miehet'}, inplace=True)
+age_group_f_counts.rename(columns={'count':'naiset'}, inplace=True)
+age_group_g_counts = pd.Series(np.sort(df['age_group'].unique())).to_frame()
+age_group_g_counts.rename(columns={0:'age_group'}, inplace=True)
+
+age_group_g_counts = pd.merge(age_group_g_counts, age_group_m_counts, on='age_group', how='outer')
+age_group_g_counts = pd.merge(age_group_g_counts, age_group_f_counts, on='age_group', how='outer')
+
+age_group_g_counts.fillna(value=0, inplace=True)
 
 
-age_group_f_counts.fillna(0, inplace=True)
-age_group_m_counts.fillna(0, inplace=True)
+categories = np.sort(df['age_group'].unique())
 
-categories = df['age_group'].unique()
+bar_pos = 0.34
 
-# Set the width of the bars
-bar_width = 0.35
+bar_width = 0.5
+bar_position=0.5
 
-# Calculate the positions of the bars
-bar_positions1 = np.arange(len(categories))
-bar_positions2 = bar_positions1 + bar_width
+ax = age_group_g_counts.plot(kind='bar', x='age_group', width=bar_width, position=bar_position, figsize=(10, 6), label='Miehet')
 
-# Plotting the bar graph
-plt.bar(bar_positions1, age_group_m_counts, width=bar_width, label='Data 1', color='blue')
-plt.bar(bar_positions2, age_group_f_counts, width=bar_width, label='Data 2', color='orange')
+plt.yticks(range(int(age_group_g_counts[['miehet','naiset']].max().max()+1)))
+plt.xlabel('Ikäryhmä')
+plt.ylabel('Lukumäärä')
 
-# Adding labels and title
-plt.xlabel('Categories')
-plt.ylabel('Values')
-plt.title('Bar Graph with Two Arrays')
-
-# Adding legend
-plt.legend()
-
-# Display the plot
 plt.show()
+
