19f53393634c06b38c1285e70d626c8badc9a960
KippaK <antti.venetjoki@gmail.com>
2024-01-26 13:24:29 +0200

    DONE

--- viikko4/tehtava4.py
+++ viikko4/tehtava4.py
@@ -4,26 +4,30 @@ import seaborn as sns
 
 df = pd.read_excel('unemployment.xlsx')
 
+
 df_period = df.loc[:,['Period', 'Unemployed']].groupby('Period').sum().reset_index()
 
 sns.lineplot(df_period, x='Period', y='Unemployed')
 plt.ticklabel_format(style='plain', axis='y')
 plt.show()
 
+
 df_period_g = df.loc[:,['Period', 'Unemployed', 'Gender']].groupby(['Period', 'Gender']).sum().reset_index()
-df_period_f = df_period_g[df_period_g['Gender']=='Women']
-df_period_m = df_period_g[df_period_g['Gender']=='Men']
-df_period_g = pd.merge(df_period_m, df_period_f, on='Period', suffixes=('Men','Women'))
-df_period_g = df_period_g.loc[:,['Period','UnemployedMen','UnemployedWomen']]
+genders = ['Men', 'Women']
+for g in genders:
+    sns.lineplot(df_period_g[df_period_g['Gender']==g], x='Period',y='Unemployed',label=g)
 
-df_period_g.rename({'UnemployedWomen':'Women','UnemployedMen':'Men'}, axis=1, inplace=True)
-
-sns.lineplot(df_period_g,x='Period',y='Men', label='Men')
-sns.lineplot(df_period_g,x='Period',y='Women', label='Women')
 plt.ticklabel_format(style='plain', axis='y')
 plt.ylabel('Unemployed')
 plt.show()
 
-# Viimeinen
-# Jokaiselle ikäryhmälle oma viiva
-# df_period_a = df.loc[.,['Period','Unemployed','Age']].groupby([''])
+
+df_period_a = df.loc[:,['Period', 'Age', 'Unemployed']].groupby(['Period','Age']).sum().reset_index()
+age_groups = ['16 to 19 years', '20 to 24 years', '25 to 34 years', '35 to 44 years', '45 to 54 years', '55 to 64 years', '65 years and over']
+
+for age in age_groups:
+    sns.lineplot(df_period_a[df_period_a['Age']==age], x='Period',y='Unemployed',label=age)
+
+plt.ticklabel_format(style='plain', axis='y')
+plt.ylabel('Unemployed')
+plt.show()
